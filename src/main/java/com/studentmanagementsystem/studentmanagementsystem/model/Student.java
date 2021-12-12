e8f15fbde42811743b869fb40d72a026fb6d39c0
marinel <marinel.bosna00@e-uvt.ro>
2021-12-12 18:42:05 +0200

    entity + connect with database

---
+++ src/main/java/com/studentmanagementsystem/studentmanagementsystem/model/Student.java
@@ -0,0 +1,31 @@
+package com.studentmanagementsystem.studentmanagementsystem.model;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name = "student")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table
+public class Student implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    private String email;
+
+    private String phone;
+
+    @Column
+    private Long studentCode;
+
+}
